554268628dc36d68b39bf4547e57f976b3790d61
JinHeeWond <162394591+JinHeeWond@users.noreply.github.com>
2026-02-17 12:28:58 +0900

    Title: 등차수열, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

--- 260217/등차수열/arithmetic-sequence.cpp
+++ 260217/등차수열/arithmetic-sequence.cpp
@@ -24,7 +24,7 @@ int main() {
         count=0;
         for(int i=0; i<n; i++){
             for(int j=i+1; j<n; j++){
-                if(((a[i]+a[j])/2)==k) count++;
+                if (a[i] + a[j] == 2 * k) count++;
             }
         }
         ans=max(ans,count);
