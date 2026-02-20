1d44a8fd96d9059dbd545c9a6cef10966c5c84dd
JinHeeWond <162394591+JinHeeWond@users.noreply.github.com>
2026-02-21 08:26:24 +0900

    Title: 연속된 숫자 만들기 2, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

--- 260221/연속된 숫자 만들기 2/create-consecutive-numbers-2.cpp
+++ 260221/연속된 숫자 만들기 2/create-consecutive-numbers-2.cpp
@@ -11,8 +11,11 @@ int main() {
     // Please write your code here.
     sort(pos, pos + 3);
 
-    if(pos[2]-pos[1]==1 && pos[1]-pos[0]==1) cout << 0;
-    else if((pos[2]-pos[1]<=2 && pos[1]-pos[0]<=2)) cout << 1;
+    int gap1 = pos[1] - pos[0];
+    int gap2 = pos[2] - pos[1];
+
+    if (gap1 == 1 && gap2 == 1) cout << 0;
+    else if (gap1 == 2 || gap2 == 2) cout << 1;
     else cout << 2;
 
     return 0;
