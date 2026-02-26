29d7399974524a2c17708aced8a44f00f4bce413
JinHeeWond <162394591+JinHeeWond@users.noreply.github.com>
2026-02-26 10:15:43 +0900

    Title: 순위 경쟁2, Time: 8ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 260226/순위 경쟁2/ranking-competition2.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+
+using namespace std;
+
+int n;
+char c[100];
+int s[100];
+
+int main() {
+    cin >> n;
+    for (int i = 0; i < n; i++) {
+        cin >> c[i] >> s[i];
+    }
+
+    // Please write your code here.
+    int A=0;
+    int B=0;
+    int change=0;
+    int prevState=-1;
+
+    for(int i=0; i<n; i++){
+        if(c[i]=='A') A+=s[i];
+        else B+=s[i];
+
+        int curState=0;
+        if(A>B) curState=0;
+        else if(B>A) curState=1;
+        else curState=2;
+
+        if(curState!=prevState){
+            change++;
+            prevState=curState;
+        }
+    }
+    cout << change;
+
+    return 0;
+}
