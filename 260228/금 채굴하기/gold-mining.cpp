7c3728412865da04392901d51382e7126aef23d0
JinHeeWond <162394591+JinHeeWond@users.noreply.github.com>
2026-02-28 12:33:47 +0900

    Title: 금 채굴하기, Time: 8ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 260228/금 채굴하기/gold-mining.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+
+using namespace std;
+
+int n, m;
+int grid[20][20];
+
+int main() {
+    cin >> n >> m;
+
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            cin >> grid[i][j];
+        }
+    }
+
+    // Please write your code here.
+    int ans=-1;
+
+    for(int r=0; r<n; r++){
+        for(int c=0; c<n; c++){
+            for(int k=0; k<n; k++){
+                int gold=0;
+                for(int i=0; i<n; i++){
+                    for(int j=0; j<n; j++){
+                        if(abs(i-r)+abs(j-c)<=k){
+                            gold+=grid[i][j];
+                        }
+                    }
+                }
+                int cost=k*k+(k+1)*(k+1);
+                if(gold*m>=cost){
+                    ans=max(ans,gold);
+                }
+            }
+        }
+    }
+
+    cout << ans;
+
+    return 0;
+}
