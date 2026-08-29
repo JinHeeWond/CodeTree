6d0ad652fcb67a6efd02fd1217dbd8d502d83662
JinHeeWond <162394591+JinHeeWond@users.noreply.github.com>
2026-08-29 10:07:39 +0900

    [Passed] 최대 동전 거슬러주기 (2ms, 0MB)

---
+++ trail4/최대 동전 거슬러주기/max-coin-change.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+
+using namespace std;
+
+int N, M;
+int coin[100];
+int dp[10001];
+
+int main() {
+    cin >> N >> M;
+    for (int i = 0; i < N; i++) {
+        cin >> coin[i];
+    }
+
+    // Please write your code here.
+    dp[0]=0;
+
+    for(int i=1; i<=M; i++){
+        dp[i]=-10000;
+        for(int j=0; j<N; j++){
+            if(i>=coin[j]){
+                if(dp[i-coin[j]]==-10000) continue;
+                dp[i]=max(dp[i],dp[i-coin[j]]+1);
+            }
+        }
+    }
+
+    int ans = dp[M];
+    if(ans==-10000) ans=-1;
+
+    cout << ans;
+    
+
+    return 0;
+}
