5db16315bbd6d1d9acfa8483ad1be8473350c98a
JinHeeWond <162394591+JinHeeWond@users.noreply.github.com>
2026-09-11 14:30:08 +0900

    [Passed] 데이터 비교 (75ms, 2MB)

---
+++ trail5/데이터 비교/data-comparison.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <unordered_set>
+
+using namespace std;
+
+int n, m;
+int arr1[100000];
+int arr2[100000];
+
+int main() {
+    cin >> n;
+    for (int i = 0; i < n; i++) {
+        cin >> arr1[i];
+    }
+
+    cin >> m;
+    for (int i = 0; i < m; i++) {
+        cin >> arr2[i];
+    }
+
+    // Please write your code here.
+    unordered_set<int> s;
+
+    for(int i=0; i<n; i++){
+        s.insert(arr1[i]);
+    }
+
+    for(int i=0; i<m; i++){
+        if(s.find(arr2[i])!=s.end()){
+            cout << 1 << " ";
+        }else{
+            cout << 0 << " ";
+        }
+    }
+
+
+    return 0;
+}
